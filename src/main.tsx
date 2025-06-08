d19b43940b191a8a7b0305ebb69dd7fe59efb7f0
Dedomenic0 <eduardo.dedomenico@gmail.com>
2025-06-08 11:44:23 -0300

    add page custom counter end finishing

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
@@ -26,5 +27,7 @@ const router = createBrowserRouter([
 ])
 
 createRoot(document.getElementById('root')!).render(
+  <StrictMode>
     <RouterProvider router={router}/>
+  </StrictMode>,
 )
